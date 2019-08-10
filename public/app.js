5b20396ee6f7a6490a8a5e521004de59a0fbdeb0
Vyacheslav Sergin <vyacheslavsergin@gmail.com>
2019-08-10 22:14:09 +0500

    Added change to code for testing merge conflict

--- public/app.js
+++ public/app.js
@@ -1,20 +1,20 @@
-document.querySelectorAll('.price').forEach(node => {
+document.querySelectorAll('.price').forEach(node => { // vyacheslavsergin
   node.textContent = new Intl.NumberFormat('ru-RU', {
     currency: 'rub',
     style: 'currency'
   }).format(node.textContent)
 })
 
-const $card = document.querySelector('#card')
+const $card = document.querySelector('#card') // vyacheslavsergin
 if ($card) {
   $card.addEventListener('click', event => {
-    if (event.target.classList.contains('js-remove')) {
+    if (event.target.classList.contains('js-remove')) { // vyacheslavsergin
       const id = event.target.dataset.id
       console.log('id', id)
 
       fetch('/card/remove/' + id, {
         method: 'delete'
-      }).then(res => res.json())
+      }).then(res => res.json()) // vyacheslavsergin
         .then(card => {
           console.log(card)
         })
