c7ade154e7429ffc34777440686c909df0aa0ba6
Vyacheslav Sergin <vyacheslavsergin@gmail.com>
2019-08-10 21:12:18 +0500

    Processing asynchronous requests

--- public/app.js
+++ public/app.js
@@ -4,3 +4,20 @@ document.querySelectorAll('.price').forEach(node => {
     style: 'currency'
   }).format(node.textContent)
 })
+
+const $card = document.querySelector('#card')
+if ($card) {
+  $card.addEventListener('click', event => {
+    if (event.target.classList.contains('js-remove')) {
+      const id = event.target.dataset.id
+      console.log('id', id)
+
+      fetch('/card/remove/' + id, {
+        method: 'delete'
+      }).then(res => res.json())
+        .then(card => {
+          console.log(card)
+        })
+    }
+  })
+}
--- routes/card.js
+++ routes/card.js
@@ -9,8 +9,17 @@ router.post('/add', async (req, res) => {
   res.redirect('/card')
 })
 
+router.delete('/remove/:id', async (req, res) => {
+  const card = await Card.remove(req.params.id)
+  res.status(200).json(card)
+})
+
 router.get('/', async (req, res) => {
   const card = await Card.fetch()
+
+  console.log('card', card)
+  // console.log('price', card.price)
+
   res.render('card', {
     title: 'Корзина',
     isCard: true,
