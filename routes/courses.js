5aa220ed23d24ca6a98004fa3a442511c90f37ff
Vyacheslav Sergin <vyacheslavsergin@gmail.com>
2019-08-10 12:18:32 +0500

    Dynamic parameters

---
+++ routes/courses.js
@@ -0,0 +1,23 @@
+const {Router} = require('express')
+const Course = require('../models/course')
+const router = Router()
+
+router.get('/', async (req, res) => {
+  const courses = await Course.getAll()
+  res.render('courses', {
+    title: 'Курсы',
+    isCourses: true,
+    courses
+  })
+})
+
+router.get('/:id', async (req, res) => {
+  const course = await Course.getById(req.params.id)
+  res.render('course', {
+    layout: 'empty',
+    title: `Курс ${course.title}`,
+    course
+  })
+})
+
+module.exports = router
