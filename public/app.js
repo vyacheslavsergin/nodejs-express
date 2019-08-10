89bd87a2ec0ea13790d126368f91741b07c5918f
Vyacheslav Sergin <vyacheslavsergin@gmail.com>
2019-08-10 21:54:59 +0500

    Dynamic change of the basket

--- public/app.js
+++ public/app.js
@@ -1,8 +1,12 @@
-document.querySelectorAll('.price').forEach(node => {
-  node.textContent = new Intl.NumberFormat('ru-RU', {
+const toCurrency = price => {
+  return new Intl.NumberFormat('ru-RU', {
     currency: 'rub',
     style: 'currency'
-  }).format(node.textContent)
+  }).format(price)
+}
+
+document.querySelectorAll('.price').forEach(node => {
+  node.textContent = toCurrency(node.textContent)
 })
 
 const $card = document.querySelector('#card')
@@ -17,6 +21,24 @@ if ($card) {
       }).then(res => res.json())
         .then(card => {
           console.log(card)
+
+          if (card.courses.length) {
+            const html = card.courses.map(c => {
+              return `
+                <tr>
+                  <td>${c.title}</td>
+                  <td>${c.count}</td>
+                  <td>
+                    <button class="btn btn-small js-remove" data-id="${c.id}">Удалить</button>
+                  </td>
+                </tr>
+              `
+            }).join('')
+            $card.querySelector('tbody').innerHTML = html
+            $card.querySelector('.price').textContent = toCurrency(card.price)
+          } else {
+            $card.innerHTML = '<p>Корзина пуста</p>'
+          }
         })
     }
   })
