14570b164759715c68b2c28d005b5fdc032a0749
Vyacheslav Sergin <vyacheslavsergin@gmail.com>
2019-08-10 17:06:34 +0500

    Basket model

--- routes/card.js
+++ routes/card.js
@@ -9,13 +9,14 @@ router.post('/add', async (req, res) => {
   res.redirect('/card')
 })
 
-router.get('/', async () => {
+router.get('/', async (req, res) => {
   const card = await Card.fetch()
   res.render('card', {
     title: 'Корзина',
-    card
+    isCard: true,
+    courses: card.courses,
+    price: card.price
   })
-
 })
 
 module.exports = router
