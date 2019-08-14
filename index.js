795d07b97fd7f1ed2e8e6c7e6637e98062e5b2b3
Vyacheslav Sergin <vyacheslavsergin@gmail.com>
2019-08-14 13:53:18 +0500

    feature/Adding-user completed

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const homeRoutes = require('./routes/home')
 const cardRoutes = require('./routes/card')
 const addRoutes = require('./routes/add')
 const coursesRoutes = require('./routes/courses')
+const User = require('./models/user')
 
 const app = express()
 
@@ -17,6 +18,16 @@ const hbs = exphbs.create({
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
+
+app.use(async (req, res, next) => {
+  try {
+    const user = await User.findById('5d53ad6d490bf2284400dceb')
+    req.user = user
+    next()
+  } catch (e) {
+    console.log(e)
+  }
+})
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
@@ -36,6 +47,18 @@ async function start() {
       useNewUrlParser: true,
       useFindAndModify: false
     })
+
+    const candidate = await User.findOne()
+
+    if (!candidate) {
+      const user = new User({
+        email: 'vyacheslavsergin@gmail.com',
+        name: 'Vyacheslav',
+        cart: { items: [] }
+      })
+
+      await user.save()
+    }
 
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`)
