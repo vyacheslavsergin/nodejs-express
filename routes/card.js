7c2ce1eef897db780c0ae40d8470614b9080d2c9
Vyacheslav Sergin <vyacheslavsergin@gmail.com>
2019-08-16 11:31:19 +0500

    Delete from cart completed

--- routes/card.js
+++ routes/card.js
@@ -21,8 +21,14 @@ router.post('/add', async (req, res) => {
 })
 
 router.delete('/remove/:id', async (req, res) => {
-  const card = await Card.remove(req.params.id)
-  res.status(200).json(card)
+  await req.user.removeFromCart(req.params.id)
+  const user = await req.user.populate('cart.items.courseId').execPopulate()
+
+  const courses = mapCartItems(user.cart)
+  const cart = {
+    courses, price: computePrice(courses)
+  }
+  res.status(200).json(cart)
 })
 
 router.get('/', async (req, res) => {
