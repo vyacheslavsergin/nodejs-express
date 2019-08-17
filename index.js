180514d08c87cfe31daae963b5d21005f7232ead
Vyacheslav Sergin <vyacheslavsergin@gmail.com>
2019-08-17 10:01:53 +0500

    Login page

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const cardRoutes = require('./routes/card')
 const addRoutes = require('./routes/add')
 const ordersRoutes = require('./routes/orders')
 const coursesRoutes = require('./routes/courses')
+const authRoutes = require('./routes/auth')
 const User = require('./models/user')
 
 const app = express()
@@ -39,6 +40,7 @@ app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes)
 app.use('/card', cardRoutes)
 app.use('/orders', ordersRoutes)
+app.use('/auth', authRoutes)
 
 const PORT = process.env.PORT || 3000
 
