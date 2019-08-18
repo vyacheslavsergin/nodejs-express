2b1e6eef0c4cb69ea48a0da7ec317d20bbacccd8
Vyacheslav Sergin <vyacheslavsergin@gmail.com>
2019-08-18 22:45:29 +0500

    Password encryption

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const { Router } = require('express')
+const bcrypt = require('bcryptjs')
 const User = require('../models/user')
 const router = Router()
 
@@ -23,7 +24,8 @@ router.post('/login', async (req, res) => {
     const candidate = await User.findOne({ email })
 
     if (candidate) {
-      const areSame = password === candidate.password
+      // const areSame = password === candidate.password
+      const areSame = await bcrypt.compare(password, candidate.password)
 
       if (areSame) {
         req.session.user = candidate
@@ -63,8 +65,12 @@ router.post('/register', async (req, res) => {
     if (candidate) {
       res.redirect('/auth/login#register')
     } else {
+      const hashPassword = await bcrypt.hash(password, 10)
       const user = new User({
-        email, name, password, cart: { items: [] }
+        email,
+        name,
+        password: hashPassword,
+        cart: { items: [] }
       })
       await user.save()
       res.redirect('/auth/login#login')
