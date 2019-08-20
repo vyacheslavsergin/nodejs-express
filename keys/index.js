6b4d8df994309dba5184dfa34bba8ed236fe4186
Vyacheslav Sergin <vyacheslavsergin@gmail.com>
2019-08-20 14:49:21 +0500

    Setting up the mail service

--- keys/index.js
+++ keys/index.js
@@ -1,4 +1,5 @@
 module.exports = {
   MONGODB_URI: 'mongodb://Vyacheslav:asus1234@ds161740.mlab.com:61740/shop',
-  SESSION_SECRET: 'some secret value'
+  SESSION_SECRET: 'some secret value',
+  SENDGRID_API_KEY: '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'
 }
