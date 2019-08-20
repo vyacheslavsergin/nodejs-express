82d9c3dacfb07a7ffd2c511cd14605ad42812b53
Vyacheslav Sergin <vyacheslavsergin@gmail.com>
2019-08-20 16:02:50 +0500

    Sending letter

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,15 @@
 const { Router } = require('express')
 const bcrypt = require('bcryptjs')
+const nodemailer = require('nodemailer')
+const sendgrid = require('nodemailer-sendgrid-transport')
 const User = require('../models/user')
+const keys = require('../keys')
+const regEmail = require('../emails/registration')
 const router = Router()
+
+const transporter = nodemailer.createTransport(sendgrid({
+  auth: { api_key: keys.SENDGRID_API_KEY }
+}))
 
 router.get('/login', async (req, res) => {
   res.render('auth/login', {
@@ -78,6 +86,7 @@ router.post('/register', async (req, res) => {
         cart: { items: [] }
       })
       await user.save()
+      await transporter.sendMail(regEmail(email))
       res.redirect('/auth/login#login')
     }
   } catch (e) {
