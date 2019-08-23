97dff324e1aa73aeed1c318afa262c3e230421bc
Vyacheslav Sergin <vyacheslavsergin@gmail.com>
2019-08-23 20:19:10 +0500

    Async validators

--- routes/auth.js
+++ routes/auth.js
@@ -75,30 +75,23 @@ router.post('/login', async (req, res) => {
 
 router.post('/register', registerValidators, async (req, res) => {
   try {
-    const { email, password, confirm, name } = req.body
-    const candidate = await User.findOne({ email })
+    const { email, password, name } = req.body
 
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
       req.flash('registerError', errors.array()[0].msg)
       return res.status(422).redirect('/auth/login#register')
     }
-
-    if (candidate) {
-      req.flash('registerError', 'Пользователь с таким email уже существует')
-      res.redirect('/auth/login#register')
-    } else {
-      const hashPassword = await bcrypt.hash(password, 10)
-      const user = new User({
-        email,
-        name,
-        password: hashPassword,
-        cart: { items: [] }
-      })
-      await user.save()
-      await transporter.sendMail(regEmail(email))
-      res.redirect('/auth/login#login')
-    }
+    const hashPassword = await bcrypt.hash(password, 10)
+    const user = new User({
+      email,
+      name,
+      password: hashPassword,
+      cart: { items: [] }
+    })
+    await user.save()
+    await transporter.sendMail(regEmail(email))
+    res.redirect('/auth/login#login')
   } catch (e) {
     console.log(e)
   }
