3b003c972c08ae9bb53e6a794f303496f35d69c0
Vyacheslav Sergin <vyacheslavsergin@gmail.com>
2019-08-25 09:51:52 +0500

    Protection of keys.

---
+++ keys/keys.dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  MONGODB_URI: 'mongodb://Vyacheslav:asus1234@ds161740.mlab.com:61740/shop',
+  SESSION_SECRET: 'some secret value',
+  SENDGRID_API_KEY: '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',
+  EMAIL_FROM: 'nodejs@email.ru',
+  BASE_URL: 'http://localhost:3000'
+}
